3e6d16cecfe1539a228b598ca1ee954203e3a44c
Junet <bebanstoreserver@gmail.com>
2024-10-14 00:16:07 +0700

    Fix some validation

--- validations/inVoiceChannel.js
+++ validations/inVoiceChannel.js
@@ -1,13 +1,14 @@
 // This makes the command cannot be executed when member is not in a vc
-module.exports = async ({ interaction, commandObj }) => {
-    const memberChannel = await interaction.member.voice.channel;
-    if (commandObj.inVoice && (!memberChannel || memberChannel === undefined)) {
-        interaction.reply({
-            content: `\`❌\` | You must be in a voice channel to use this command.`,
-            ephemeral: true,
-        });
-        // This stops the command from being executed.
+module.exports = ({ interaction, commandObj }) => {
+    const memberChannel = interaction.member.voice.channelId
+    if (commandObj.options.inVoice) {
+        if (!memberChannel) {
+            interaction.reply({
+                content: `\`❌\` | You must be in a voice channel to use this command.`,
+                ephemeral: true,
+            });
+        // This must be added to stop the command from being executed.
         return true;
+        }
     }
 };
-
--- validations/sameVoiceChannel.js
+++ validations/sameVoiceChannel.js
@@ -1,13 +1,9 @@
 // This makes the command cannot be executed when member is not on the same vc as the bot
-module.exports = (interaction, commandObj) => {
+module.exports = ({ interaction, commandObj, client }) => {
     const memberChannel = interaction.member.voice.channel
 	const player = client.riffy.players.get(interaction.guildId);
-    if (commandObj.sameVoice) {
-		if (!player) {
-			return interaction.reply({ content: "\`❌\` | No active player found.", ephemeral: true });
-		}
-
-		if (player.voiceChannel !== memberChannel) {
+    if (commandObj.options.sameVoice) {
+		if (player && player.voiceChannel !== memberChannel) {
 			return interaction.reply({ content: "\`❌\` | You must be in the same voice channel as the bot.", ephemeral: true });
 		}
     } 
