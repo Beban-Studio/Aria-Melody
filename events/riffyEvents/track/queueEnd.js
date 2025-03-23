7a60a714a89db9c51aa3e52cbb8709fa3233502d
Junet <bebanstoreserver@gmail.com>
2025-03-23 13:06:22 +0700

    Delete the queueEnd message after a few seconds

--- events/riffyEvents/track/queueEnd.js
+++ events/riffyEvents/track/queueEnd.js
@@ -1,3 +1,4 @@
+const parseTimeString = require("../../../utils/parseTimeString");
 const client = require("../../../Aria");
 
 client.riffy.on("queueEnd", async (player) => {
@@ -6,9 +7,13 @@ client.riffy.on("queueEnd", async (player) => {
     if (player.message) await player.message.delete().catch((e) => {});
 
     if (player.isAutoplay === true) {
-        player.autoplay(player)
+        player.autoplay(player);
     } else {
         player.destroy();
-        channel.send("\`⌛️\` | Queue has ended.");
+        const message = await channel.send("\`⌛️\` | Queue has ended.");
+        
+        setTimeout(() => {
+            message.delete().catch((e) => {});
+        }, parseTimeString("30s"));
     }
 });
