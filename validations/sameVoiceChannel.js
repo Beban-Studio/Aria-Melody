a4b7eba48cbbab15bd2d2c57eafde745eeebc19c
Junet <bebanstoreserver@gmail.com>
2025-04-02 23:12:33 +0700

    Fix validation not checking if interaction deferred or not

--- validations/sameVoiceChannel.js
+++ validations/sameVoiceChannel.js
@@ -1,13 +1,33 @@
 // This makes the command cannot be executed when member is not on the same vc as the bot
 module.exports = ({ interaction, commandObj, client }) => {
     if (commandObj.options?.sameVoice) {
+        if (interaction.isAutocomplete() || interaction.isButton()) return;
+        
         const memberChannel = interaction.member.voice.channel;
         const player = client.riffy.players.get(interaction.guildId);
-        
         if (player) {
             if (player.voiceChannel !== memberChannel.id) {
+                if (interaction.deferred) {
+                    return interaction.editReply({ 
+                        content: "\`❌\` | You must be in the same voice channel as the bot.", 
+                        ephemeral: true 
+                    });
+                } else {
+                    return interaction.reply({ 
+                        content: "\`❌\` | You must be in the same voice channel as the bot.", 
+                        ephemeral: true 
+                    });
+                }
+            }
+        } else {
+            if (interaction.deferred) {
+                return interaction.editReply({ 
+                    content: "\`❌\` | No music is currently playing.", 
+                    ephemeral: true 
+                });
+            } else {
                 return interaction.reply({ 
-                    content: "\`❌\` | You must be in the same voice channel as the bot.", 
+                    content: "\`❌\` | No music is currently playing.", 
                     ephemeral: true 
                 });
             }
