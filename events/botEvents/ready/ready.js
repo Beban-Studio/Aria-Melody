ea24839b21ed6a246694f814cf5ffb149bb6f3ae
Junet <bebanstoreserver@gmail.com>
2025-04-07 23:47:33 +0700

    Remove string.js

--- events/botEvents/ready/ready.js
+++ events/botEvents/ready/ready.js
@@ -1,25 +1,34 @@
-const { capitalize, format } = require("../../../utils/string");
 const { ActivityType } = require("discord.js");
 const { logger } = require("../../../utils/logger");
 const config = require("../../../config");
 const colors = require("colors");
 
 module.exports = (client) => {
-	logger(`Successfully logged in as ${colors.rainbow(`[${client.user.tag}]`)}`, "debug");
+    logger(`Successfully logged in as ${colors.rainbow(`[${client.user.tag}]`)}`, "debug");
 
-   	const activities = config.presence.activities;
-   	client.user.setStatus(config.presence.status);
-	setInterval(() => {
-		const index = Math.floor(Math.random() * activities.length);
+    const activities = config.presence.activities;
+    client.user.setStatus(config.presence.status);
+    
+    setInterval(() => {
+        const index = Math.floor(Math.random() * activities.length);
 
-		let data = {};
-		try {
-			data = activities[index].data(client);
-		} catch (err) {}
+        let data = {};
+        try {
+            data = activities[index].data(client);
+        } catch (err) {}
 
-		client.user.setActivity({
-			name: format(activities[index].name, data),
-			type: ActivityType[capitalize(activities[index].type)],
-		});
-	}, 10000);
-}; 
+        const format = (template, data) => {
+            return template.replace(/\{(\w+)\}/g, (_, key) => data[key] || '');
+        };
+
+        const capitalize = (str) => {
+            if (!str) return '';
+            return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+        };
+
+        client.user.setActivity({
+            name: format(activities[index].name, data),
+            type: ActivityType[capitalize(activities[index].type)],
+        });
+    }, 10000);
+};
