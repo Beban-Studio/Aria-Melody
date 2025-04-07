e48461dc88ca0ff45c10fe636c252bb7216eb5f8
Junet <bebanstoreserver@gmail.com>
2025-04-07 23:42:33 +0700

    Change console.log to logger

--- events/botEvents/ready/antiCrash.js
+++ events/botEvents/ready/antiCrash.js
@@ -1,26 +1,32 @@
 const { logger } = require("../../../utils/logger");
 
 module.exports = async () => {
-    process.on("unhandledRejection", async (reason, promise) => {
-        console.log("[AntiCrash] | [UnhandledRejection_Logs] | [START] :");
-        console.log("Unhandled Rejection at:", promise);
-        console.log("reason:", reason);
-        console.log("[AntiCrash] | [UnhandledRejection_Logs] | [END] :");
+    process.on("unhandledRejection", (reason, promise) => {
+        logger(`[AntiCrash] | [UnhandledRejection_Logs] | [START] :`, "error");
+        logger(`Unhandled Rejection at: ${promise}`, "error");
+        logger(`Reason: ${reason}`, "error");
+        logger(`[AntiCrash] | [UnhandledRejection_Logs] | [END] :`, "error");
+        
+        // Optionally exit the process
+        // process.exit(1);
     });
 
-    process.on("uncaughtException", async (err, origin) => {
-        console.log("[AntiCrash] | [UncaughtException_Logs] | [START] :");
-        console.log(`Uncaught exception: ${err}`);
-        console.log(`Exception origin: ${origin}`);
-        console.log("[AntiCrash] | [UncaughtException_Logs] | [END] :");
+    process.on("uncaughtException", (err, origin) => {
+        logger(`[AntiCrash] | [UncaughtException_Logs] | [START] :`, "error");
+        logger(`Uncaught exception: ${err}`, "error");
+        logger(`Exception origin: ${origin}`, "error");
+        logger(`[AntiCrash] | [UncaughtException_Logs] | [END] :`, "error");
+        
+        // Optionally exit the process
+        // process.exit(1);
     });
 
-    process.on("uncaughtExceptionMonitor", async (err, origin) => {
-        console.log("[AntiCrash] | [UncaughtExceptionMonitor_Logs] | [START] :");
-        console.log(`Uncaught exception monitor: ${err}`);
-        console.log(`Exception origin: ${origin}`);
-        console.log("[AntiCrash] | [UncaughtExceptionMonitor_Logs] | [END] :");
+    process.on("uncaughtExceptionMonitor", (err, origin) => {
+        logger(`[AntiCrash] | [UncaughtExceptionMonitor_Logs] | [START] :`, "error");
+        logger(`Uncaught exception monitor: ${err}`, "error");
+        logger(`Exception origin: ${origin}`, "error");
+        logger(`[AntiCrash] | [UncaughtExceptionMonitor_Logs] | [END] :`, "error");
     });
 
-    logger("Successfully loadded Anticrash");
+    logger("Successfully loaded AntiCrash", "debug");
 };
