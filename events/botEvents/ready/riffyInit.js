736f92674c20af4551ab62d08d08f2a86f8c9e5f
Junet <bebanstoreserver@gmail.com>
2025-04-07 23:48:01 +0700

    Move riffy init to Aria.js

--- events/botEvents/ready/riffyInit.js
+++
@@ -1,8 +0,0 @@
-const { client_id } = require("../../../config");
-const { logger } = require("../../../utils/logger");
-const client = require("../../../Aria");
-
-module.exports = async () => {
-    client.riffy.init(client_id)
-    logger(`Successfully Initiate Riffy Events`, "debug");
-};
