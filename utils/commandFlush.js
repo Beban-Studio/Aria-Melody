c1c35efd7dda0d88d277c52805945d9ed2bab342
BebanCode <bebanstudios@gmail.com>
2025-05-19 00:56:12 +0700

    Fix stuck at executing flushcommand

--- utils/commandFlush.js
+++ utils/commandFlush.js
@@ -1,11 +1,19 @@
-const { REST } = require("@discordjs/rest");
 const { clientOptions } = require("../config");
 const { Routes } = require("discord-api-types/v10");
+const { logger } = require("./logger");
+const { REST } = require("@discordjs/rest");
 
 (async () => {
     const rest = new REST({ version: "10" }).setToken(clientOptions.clientToken);
 
-        console.log("Bot has started to delete commands...");
-        await rest.put( Routes.applicationCommands(clientOptions.clientId), { body: [] });
-        console.log("Done");
+    logger("Bot has started to delete commands...", "warn");
+    try {
+        await rest.put(Routes.applicationCommands(clientOptions.clientId), { body: [] });
+        logger("The bot is done deleting all past commands", "success");
+    } catch (err) {
+        logger(`Error deleting commands: ${err}`, "error");
+        process.exit(1); 
+    }
+
+    process.exit(0); 
 })();
